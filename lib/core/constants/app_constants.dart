a26c0e1c10dabe9d45883fd916d21a7e33555aa2
apple <apple@apples-MacBook-Pro.local>
2026-05-01 19:33:40 +0530

    add blogs and category table

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   // Replace with your actual Supabase project URL and anon key
-  static const supabaseUrl = 'https://YOUR_PROJECT.supabase.co';
-  static const supabaseAnonKey = 'YOUR_ANON_KEY';
+  static const supabaseUrl = 'https://zlihrmonlrflsxspvnah.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsaWhybW9ubHJmbHN4c3B2bmFoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc0NjM0MTMsImV4cCI6MjA5MzAzOTQxM30.cRQYOSHIGfgYwe980yDQnbm8tMU9PK1Gmv7J4bkLP_k';
 
   // Supabase table names
   static const blogsTable = 'blogs';
